137b2d888e59cb815bbc5f48be4fc8e7c5e77178
Dawood0426 <rd535328@gmail.com>
2025-05-01 05:15:21 -0700

    add new
    new ha ye

--- components/header/header.js
+++ components/header/header.js
@@ -1,43 +1,60 @@
+"use client";
 import Link from "next/link";
 import "./header.css";
 
 export function Header() {
-    return (
-        <header>
-            <nav className="navbar navbar-expand-lg navbar-dark bg-dark py-3">
-                <div className="container-fluid">
-                    {/* Logo */}
-                    <Link href="/" className="navbar-brand d-flex align-items-center">
-                        <img src="/logo.jpg" alt="Logo" width={120} height={40} className="me-2" />
-                    </Link>
+  // Close mobile menu after clicking a nav link
+  const closeMobileMenu = () => {
+    const navbarCollapse = document.getElementById("navbarContent");
+    if (navbarCollapse?.classList.contains("show")) {
+      new bootstrap.Collapse(navbarCollapse).hide(); 
+    }
+  };
 
-                    {/* Toggler for mobile */}
-                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarContent" aria-controls="navbarContent" aria-expanded="false" aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon"></span>
-                    </button>
+  return (
+    <header>
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark py-3">
+        <div className="container-fluid">
+          {/* Logo */}
+          <Link href="/" className="navbar-brand d-flex align-items-center" onClick={closeMobileMenu}>
+            <img src="/logo.jpg" alt="Logo" width={120} height={40} className="me-2" />
+          </Link>
 
-                    {/* Nav links */}
-                    <div className="collapse navbar-collapse justify-content-end" id="navbarContent">
-                        <ul className="navbar-nav gap-lg-4 gap-2 align-items-center">
-                            <li className="nav-item ak-khas">
-                                <Link className="nav-link another-level fs-5 text-white" href="/">Home</Link>
-                            </li>
-                            <li className="nav-item ak-khas">
-                                <Link className="nav-link another-level fs-5 text-white" href="/login">Login</Link>
-                            </li>
-                            <li className="nav-item ak-khas">
-                                <Link className="nav-link another-level fs-5 text-white" href="/products">Products</Link>
-                            </li>
-                            <li className="nav-item ak-khas">
-                                <Link className="nav-link another-level fs-5 text-white" href="/signup">Signup</Link>
-                            </li>
-                            <li className="nav-item ak-khas">
-                                <Link className="nav-link another-level  fs-5 text-white" href="/cart">Cart</Link>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-            </nav>
-        </header>
-    );
+          {/* Toggler for mobile */}
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarContent"
+            aria-controls="navbarContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+
+          {/* Nav links */}
+          <div className="collapse navbar-collapse justify-content-end" id="navbarContent">
+            <ul className="navbar-nav gap-lg-4 gap-2 align-items-center">
+              <li className="nav-item ak-khas">
+                <Link href="/" className="nav-link another-level fs-5 text-white" onClick={closeMobileMenu}>Home</Link>
+              </li>
+              <li className="nav-item ak-khas">
+                <Link href="/login" className="nav-link another-level fs-5 text-white" onClick={closeMobileMenu}>Login</Link>
+              </li>
+              <li className="nav-item ak-khas">
+                <Link href="/products" className="nav-link another-level fs-5 text-white" onClick={closeMobileMenu}>Products</Link>
+              </li>
+              <li className="nav-item ak-khas">
+                <Link href="/signup" className="nav-link another-level fs-5 text-white" onClick={closeMobileMenu}>Signup</Link>
+              </li>
+              <li className="nav-item ak-khas">
+                <Link href="/cart" className="nav-link another-level fs-5 text-white" onClick={closeMobileMenu}>Cart</Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </header>
+  );
 }
